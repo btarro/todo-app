530f06b82e69e83077ec8c41367deff4f68892e5
Bruce Tarro <brucetarro@gmail.com>
2020-12-20 22:40:17 -0700

    Building core logic; can add todos

--- todoapp.js
+++ todoapp.js
@@ -1,4 +1,39 @@
 // todo app
+// ** WIP ** 12182020
 
-const newTodo = document.getElementById('newTodo');
-cost testTodo = document.getElementById('testTodo').innerHTML = newTodo;
+
+function myFunction() {
+    var newNode = document.createElement("LI");
+    var x = document.getElementById("newTodo").value;
+    var textNode = document.createTextNode(x);
+    newNode.appendChild(textNode);
+    document.getElementById("todoList").appendChild(newNode);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// todoList.forEach(myFunction)
+
+// // add item
+// function addTodo(a) {
+//     let newItem = a;
+//     todoList.push(newItem);
+//     console.log(todoList);
+// }
+
+// // remove item
+// function removeTodo(a) {
+//     let removeItem = a;
+//     todoList.splice(removeItem);
+// }
+
