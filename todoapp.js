03b55168d73e9a4f179ebd4ced1ffc7afbe23df5
Bruce Tarro <brucetarro@gmail.com>
2020-12-23 13:54:15 -0700

    flexbox; css updates

--- todoapp.js
+++ todoapp.js
@@ -1,39 +1,42 @@
 // todo app
-// ** WIP ** 12182021
 
-// This needs to be cleaned up
 function createInput() {
     const mainContainer = document.getElementById("inputDiv")
 
+    const newSubmitButton = document.createElement("INPUT");
+    newSubmitButton.setAttribute("type", "button");
+    newSubmitButton.setAttribute("value", "+");
+    newSubmitButton.setAttribute("class", "actionButton");
+    newSubmitButton.addEventListener('click', submitAndRefresh); //working here
+
     const newInput = document.createElement("INPUT");
-    newInput.setAttribute("placeholder", "What do you need to get done? ");
+    newInput.setAttribute("placeholder", "What do you need to do? ");
     newInput.setAttribute("type", "text");
     newInput.setAttribute("id", "newTodoInput");
 
-    const newSubmitButton = document.createElement("INPUT");
-    newSubmitButton.setAttribute("type", "button");
-    newSubmitButton.setAttribute("value", "Submit");
-    newSubmitButton.addEventListener('click', submitAndRefresh); //working here
-
-    mainContainer.appendChild(newInput);
     mainContainer.appendChild(newSubmitButton);
+    mainContainer.appendChild(newInput);
 
-    function submitAndRefresh() {
+    function submitAndRefresh() { // check for empty string
+
         submitNewTodo();
         newInput.value = '';
     };
 
 };
 
-function freshInput() {
-    submitNewTodo();
-    //createInput();
-}
-
 function submitNewTodo() {
     const newTodoItem = document.createElement("LI");
+    newTodoItem.setAttribute("class", "listElements")
     const returnTodo = document.getElementById("newTodoInput");
     const todoTextValue = document.createTextNode(returnTodo.value);
+
+    const todoDeleteButton = document.createElement("BUTTON")
+    todoDeleteButton.textContent = "x";
+    newTodoItem.appendChild(todoDeleteButton);
+    todoDeleteButton.setAttribute("class", "actionButton");
+    todoDeleteButton.addEventListener('click', removeTodo)
+
 
     newTodoItem.appendChild(todoTextValue);
     newTodoItem.addEventListener('click', toggleStrike);
@@ -42,10 +45,7 @@ function submitNewTodo() {
         newTodoItem.classList.toggle("strikeMeDown");
     };
 
-    const todoDeleteButton = document.createElement("BUTTON")
-    todoDeleteButton.textContent = "Delete";
-    newTodoItem.appendChild(todoDeleteButton);
-    todoDeleteButton.addEventListener('click', removeTodo)
+
 
     function removeTodo() {
         newTodoItem.remove();
@@ -53,34 +53,6 @@ function submitNewTodo() {
 
     document.getElementById("todoList").appendChild(newTodoItem);
 };
-
-
-// function submitNewTodo() {
-//     const newTodoItem = document.createElement("LI");
-//     const returnTodo = document.getElementById("newTodoInput");
-//     const todoTextValue = document.createTextNode(returnTodo.value);
-
-//     newTodoItem.appendChild(todoTextValue);
-//     newTodoItem.addEventListener('click', toggleStrike);
-
-//     function toggleStrike() {
-//         newTodoItem.classList.toggle("strikeMeDown");
-//     };
-
-//     let btn = document.createElement("BUTTON")
-//     btn.textContent = "Delete";
-//     newTodoItem.appendChild(btn);
-//     btn.addEventListener('click', removeTodo)
-
-//     function removeTodo() {
-//         newTodoItem.remove();
-//     }
-
-//     document.getElementById("todoList").appendChild(newTodoItem);
-//     document.getElementById("todoForm").reset();  // Are we still using a form?
-//     document.getElementById("newTodoInput").reset();
-// };
-
 
 createInput();
 
