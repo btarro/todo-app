bcf5dcc03128c4d8b64300b831c0c366a5d5ddee
Bruce Tarro <brucetarro@gmail.com>
2020-12-22 21:08:51 -0700

    refactoring logic, wip

--- todoapp.js
+++ todoapp.js
@@ -1,48 +1,37 @@
 // todo app
 // ** WIP ** 12182021
 
-
-function submitNewTodo() {
-    let newNode = document.createElement("LI");
-    let x = document.getElementById("newTodo").value;
-    var textNode = document.createTextNode(x);
-    newNode.appendChild(textNode);
-    newNode.onclick = function () {
-        newNode.classList.toggle("strikeMeDown");
-        let btn = document.createElement("BUTTON")
-        btn.innerHTML = "X";
-        newNode.appendChild(btn);
-        btn.onclick = function () {
-            newNode.remove();
-        }
-    };
-    document.getElementById("todoList").appendChild(newNode);
+function createInput() {
+    const newInput = document.createElement(INPUT[placeholder = "text"]);
+    document.getElementById("inputDiv").appendChild(newInput);
 }
 
+function submitNewTodo() {
+    const newTodoItem = document.createElement("LI");
+    const returnTodo = document.getElementById("newTodoInput");
+    const todoTextValue = document.createTextNode(returnTodo.value);
+
+    newTodoItem.appendChild(todoTextValue);
+    newTodoItem.addEventListener('click', toggleStrike);
+
+    function toggleStrike() {
+        newTodoItem.classList.toggle("strikeMeDown");
+    };
+
+    let btn = document.createElement("BUTTON")
+    btn.textContent = "Delete";
+    newTodoItem.appendChild(btn);
+    btn.addEventListener('click', removeTodo)
+
+    function removeTodo() {
+        newTodoItem.remove();
+    }
+
+    document.getElementById("todoList").appendChild(newTodoItem);
+    document.getElementById("todoForm").reset();  // Are we still using a form?
+    document.getElementById("newTodoInput").reset();
+};
 
 
-
-
-
-
-
-
-
-
-
-
-// todoList.forEach(myFunction)
-
-// // add item
-// function addTodo(a) {
-//     let newItem = a;
-//     todoList.push(newItem);
-//     console.log(todoList);
-// }
-
-// // remove item
-// function removeTodo(a) {
-//     let removeItem = a;
-//     todoList.splice(removeItem);
-// }
+createInput();
 
