0a8c0efe90dd916277a9818c882df99f21fee90e
Bruce Tarro <brucetarro@gmail.com>
2020-12-24 07:49:03 -0700

    finished logic; look & feel; readme

--- README.md
+++ README.md
@@ -1,30 +1,15 @@
 ### SDMM - Todo App
 
-# Considerations / known issues
-* BUG - toggle strike through class generates extra delete buttons
-* BUG - text input does not clear/refresh after an entry
-* BUG - pressing 'enter' wipes out all items
-* FEATURE - remove submit button; accept on enter keypress
-* FEATURE (BONUS) - local storage
-* STYLING - fonts need to be adjusted
-* STYLING - List display should look a bit more elegant
+# Simple to-do app
 
-# Requirements
-    [ ] Project will be written in:
-        [X] HTML
-        [X] CSS
-        [X] Javascript
-    No Javascript libraries or frameworks are allowed
-    No following tutorials of todo apps/projects
-    No copying code of other existing todo apps/projects
+### A simple & minimalistic version of the venerable to-do app. 
 
-# Deliverables
-    [X] Project should be in a Git repository on GitHub
-    [o] You should make regular (daily) Git commits to the project
-    [X] A user should be able to see a list of current TO-DO’s
-    [X] A user should be able to make a TO-DO as completed
-    [X] When a user mark’s a TO-DO as completed the message of the TO-DO should be crossed out (using strikethrough font)
-    [X] A user should have the option of deleting TO-DO’s that have been mark completed
-    [ ] BONUS (if completed ahead of time):
-        TO-DO list should for each user between visits (using local storage)
+#### Check it out [here] (https://https://btarro.github.io/todo-app)
 
+![](images/simple_todo_example2.jpg)
+
+## Summary
+This right-of-passage todo project was built with a simple and clean aesthetic in mind. It leverages basic HTML, CSS(flexbox), and JavaScript. 
+
+## Author
+Bruce Tarro - Aspiring Software Developer [LinkedIn](https://www.linkedin.com/in/bruce-tarro/) | [Personal Website] (Comming Soon!)
--- todoapp.js
+++ todoapp.js
@@ -1,5 +1,13 @@
 // todo app
 
+
+// Notes for code review:
+// 1. Originally I was leveraging an HTML input/'onClick' but decided to us JS to build the button element completely.
+// 2. Retrospectively, I should have leveraged an array - it made using localstorage out of scope without 
+// a refactor. (maybe?)
+
+
+// Establish the todo input box & submit button logic
 function createInput() {
     const mainContainer = document.getElementById("inputDiv")
 
@@ -7,24 +15,29 @@ function createInput() {
     newSubmitButton.setAttribute("type", "button");
     newSubmitButton.setAttribute("value", "+");
     newSubmitButton.setAttribute("class", "actionButton");
-    newSubmitButton.addEventListener('click', submitAndRefresh); //working here
+    newSubmitButton.addEventListener('click', submitAndRefresh);
 
     const newInput = document.createElement("INPUT");
-    newInput.setAttribute("placeholder", "What do you need to do? ");
+    newInput.setAttribute("placeholder", "Click here to add a todo.. ");
     newInput.setAttribute("type", "text");
     newInput.setAttribute("id", "newTodoInput");
 
     mainContainer.appendChild(newSubmitButton);
     mainContainer.appendChild(newInput);
 
-    function submitAndRefresh() { // check for empty string
-
+    // input button action; validate text; input clear
+    function submitAndRefresh() {
+        let validateText = document.getElementById("newTodoInput").value;
+        if (validateText == "") {
+            return false;
+        }
         submitNewTodo();
         newInput.value = '';
     };
 
 };
 
+// Create new list element based on input from createInput function
 function submitNewTodo() {
     const newTodoItem = document.createElement("LI");
     newTodoItem.setAttribute("class", "listElements")
@@ -36,7 +49,6 @@ function submitNewTodo() {
     newTodoItem.appendChild(todoDeleteButton);
     todoDeleteButton.setAttribute("class", "actionButton");
     todoDeleteButton.addEventListener('click', removeTodo)
-
 
     newTodoItem.appendChild(todoTextValue);
     newTodoItem.addEventListener('click', toggleStrike);
